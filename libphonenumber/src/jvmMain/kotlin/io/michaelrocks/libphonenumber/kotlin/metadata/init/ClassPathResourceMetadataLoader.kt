7b0841b00e5850c7a9972b9415da5c28e073cff0
Luca Spinazzola <mspinluca@gmail.com>
2024-11-05 01:10:59 -0800

    fix jvm loadMetadata

--- libphonenumber/src/jvmMain/kotlin/io/michaelrocks/libphonenumber/kotlin/metadata/init/ClassPathResourceMetadataLoader.kt
+++ libphonenumber/src/jvmMain/kotlin/io/michaelrocks/libphonenumber/kotlin/metadata/init/ClassPathResourceMetadataLoader.kt
@@ -17,11 +17,10 @@
 package io.michaelrocks.libphonenumber.kotlin.metadata.init
 
 import co.touchlab.kermit.Logger
-import io.github.luca992.libphonenumber_kotlin.libphonenumber.generated.resources.Res
 import io.michaelrocks.libphonenumber.kotlin.MetadataLoader
 import io.michaelrocks.libphonenumber.kotlin.io.InputStream
 import io.michaelrocks.libphonenumber.kotlin.io.JavaInputStream
-import java.net.URL
+import org.jetbrains.compose.resources.MissingResourceException
 
 /**
  * A [MetadataLoader] implementation that reads phone number metadata files as classpath
@@ -29,25 +28,23 @@ import java.net.URL
  */
 class ClassPathResourceMetadataLoader : MetadataLoader {
     override fun loadMetadata(phoneMetadataResource: String): InputStream? {
-        try {
-            // `getUri` returns a string, so treat it as a path
-            val resourcePath: String = Res.getUri(phoneMetadataResource)
-            logger.d { "Loading metadata from classpath resource:\n\t$resourcePath" }
+        return JavaInputStream(
+            getResourceAsStream(
+                "composeResources/io.github.luca992.libphonenumber_kotlin.libphonenumber.generated.resources/" +
+                        phoneMetadataResource
+            )
+        )
+    }
 
-            // Convert to a URL or use class loader to access the resource
-            val resourceUrl: URL? = this::class.java.classLoader.getResource(resourcePath)
+    private fun getResourceAsStream(path: String): java.io.InputStream {
+        val classLoader = getClassLoader()
+        return classLoader.getResourceAsStream(path) ?: throw MissingResourceException(
+            path
+        )
+    }
 
-            return if (resourceUrl != null) {
-                val resourceStream = resourceUrl.openStream()
-                JavaInputStream(resourceStream)
-            } else {
-                logger.e { "Resource not found: $resourcePath" }
-                null
-            }
-        } catch (e: Exception) {
-            logger.e { "Failed to load metadata: ${e.message}" }
-            return null
-        }
+    private fun getClassLoader(): ClassLoader {
+        return this.javaClass.classLoader ?: error("Cannot find class loader")
     }
 
     companion object {
