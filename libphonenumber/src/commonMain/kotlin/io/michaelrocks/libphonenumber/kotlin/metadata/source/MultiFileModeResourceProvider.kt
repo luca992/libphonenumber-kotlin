2eb3c30ce94cce1cd24da166dcacb2cf87c92735
Lukáš Moravec <morki@morki.cz>
2025-11-19 13:37:01 +0100

    Remove debug print statement in getFor method

--- libphonenumber/src/commonMain/kotlin/io/michaelrocks/libphonenumber/kotlin/metadata/source/MultiFileModeResourceProvider.kt
+++ libphonenumber/src/commonMain/kotlin/io/michaelrocks/libphonenumber/kotlin/metadata/source/MultiFileModeResourceProvider.kt
@@ -31,7 +31,6 @@ class MultiFileModeResourceProvider(
     }
 
     override fun getFor(key: Any): String? {
-        println("MultiFileModeResourceProvider.getFor key: $key")
         val keyAsString = key.toString()
         require(ALPHANUMERIC.matchEntire(keyAsString) != null) { "Invalid key: $keyAsString" }
         return phoneMetadataFileNamePrefix + keyAsString
